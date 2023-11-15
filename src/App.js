8c74c2b8228c63741037926fa648d809b8a3e5ec
md-bappi <mdabuanasbappy555@gmail.com>
2023-11-15 12:47:22 +0600

    real time data added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,38 @@
+import { useEffect, useState } from "react";
+import AddTask from "./components/AddTask";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import TaskList from "./components/TaskList";
+
 const App = () => {
-  return <div>App</div>;
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    // getting data from the server
+    fetchingData();
+  }, []);
+
+  // fetching data
+  const fetchingData = async () => {
+    try {
+      const response = await fetch(
+        "https://angry-adaptive-hydrangea.glitch.me/tasks"
+      );
+      if (!response.ok) throw new Error("Something went wrong");
+      const data = await response.json();
+      setTasks(data);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+  return (
+    <div className="wrapper bg-gradient-to-t from-gray-900 to-teal-900 min-h-screen text-xl text-gray-100 flex flex-col py-10">
+      <Header />
+      <AddTask tasks={tasks} setTasks={setTasks} />
+      <TaskList tasks={tasks} />
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/AddTask.js
@@ -0,0 +1,61 @@
+import { useRef, useState } from "react";
+
+const AddTask = ({ tasks, setTasks }) => {
+  const [task, setTask] = useState("");
+  const inputRef = useRef(null);
+
+  // add task handler event
+  const addTaskHandler = (e) => {
+    e.preventDefault();
+
+    // post task into server
+    taskPosting(task);
+
+    inputRef.current.blur();
+    setTask("");
+  };
+  // 1.30
+  // task posting
+  // use "text"
+
+  const taskPosting = async (text) => {
+    const response = await fetch(
+      "https://angry-adaptive-hydrangea.glitch.me/tasks",
+      {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify({ text }),
+      }
+    );
+    const data = await response.json();
+    // real-time data updation
+    setTasks([...tasks, data]);
+  };
+
+  return (
+    <form
+      className="bg-gray-900 container mx-auto flex justify-between p-10"
+      onSubmit={addTaskHandler}
+    >
+      <input
+        ref={inputRef}
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
+        required
+        type="text"
+        placeholder="What things to do?"
+        className=" bg-transparent outline-none border-b-2 border-gray-400 py-2 px-5 focus:border-teal-600"
+      />
+      <button
+        type="submit"
+        className=" bg-teal-900/30 py-2 px-5 border-2 border-teal-500 rounded text-teal-500 hover:bg-teal-500 hover:text-gray-900 duration-300 capitalize"
+      >
+        Add task
+      </button>
+    </form>
+  );
+};
+
+export default AddTask;
